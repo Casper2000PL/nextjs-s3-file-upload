bbd2f82096bfa9b26163008a04912dc9c82f66fb
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-08-12 19:18:58 +0200

    4. Client-Side State

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,17 +1,42 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
 
 const Uploader = () => {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
+  const [files, setFiles] = useState<
+    Array<{
+      id: string;
+      file: File;
+      uploading: boolean;
+      progress: number;
+      key?: string;
+      isDeleting: boolean;
+      error: boolean;
+      objectUrl?: string;
+    }>
+  >([]);
 
-    console.log(acceptedFiles);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    if (acceptedFiles.length > 0) {
+      setFiles((prevFiles) => [
+        ...prevFiles,
+        ...acceptedFiles.map((file) => ({
+          id: uuidv4(),
+          file,
+          uploading: false,
+          progress: 0,
+          isDeleting: false,
+          error: false,
+          objectUrl: URL.createObjectURL(file),
+        })),
+      ]);
+    }
   }, []);
 
   const onDropRejected = useCallback((fileRejections: FileRejection[]) => {
@@ -45,28 +70,38 @@ const Uploader = () => {
   });
 
   return (
-    <Card
-      className={cn(
-        "border-dashed border-2 border-gray-300 p-4",
-        isDragActive
-          ? "border-primary bg-primary/10 border-solid"
-          : "border-border hover:border-primary "
-      )}
-      {...getRootProps()}
-    >
-      <CardContent className="flex flex-col items-center justify-center w-full h-64">
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <div className="flex flex-col items-center justify-center h-full w-full gap-y-3">
-            <p>Drag drop some files here, or click to select files</p>
-
-            <Button>Select files</Button>
-          </div>
+    <>
+      <Card
+        className={cn(
+          "border-dashed border-2 border-gray-300 p-4",
+          isDragActive
+            ? "border-primary bg-primary/10 border-solid"
+            : "border-border hover:border-primary "
         )}
-      </CardContent>
-    </Card>
+        {...getRootProps()}
+      >
+        <CardContent className="flex flex-col items-center justify-center w-full h-64">
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full w-full gap-y-3">
+              <p>Drag drop some files here, or click to select files</p>
+
+              <Button>Select files</Button>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 mt-6">
+        {files.map((file) => (
+          <div key={file.id}>
+            <img src={file.objectUrl} alt={file.file.name} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
